423676e224f8310d60ea1d0c0321c648cad9603e
Anggoro271102 <92448438+Anggoro271102@users.noreply.github.com>
2023-03-21 16:36:02 +0700

    Update Latihan1.java

--- 202110370311327_Anggoro Dwi Utomo_StrukDat Latihan 2/Latihan1.java
+++ 202110370311327_Anggoro Dwi Utomo_StrukDat Latihan 2/Latihan1.java
@@ -1,3 +1,4 @@
+
 import java.util.ArrayList;
 public class Latihan1 {
     public static void main(String[] args) throws Exception {
@@ -12,16 +13,15 @@ public class Latihan1 {
             
             System.out.println("Before");
             System.out.println(Hewan);
-            System.out.println("After");
             DeleteHewan.addAll(Hewan);
-            int hapus=1;
-            int hapus1=2;
-            int hapussaja=3;
             System.out.println("Hewan Yang Dihapus\t: ");
-            System.out.println(DeleteHewan.get(hapus)+","+DeleteHewan.get(hapus1)+","+DeleteHewan.get(hapussaja)+"");
-            DeleteHewan.remove(hapus);
-            DeleteHewan.remove(hapus1);
-            DeleteHewan.remove("Unta");
+            System.out.println(DeleteHewan.get(1)+","+DeleteHewan.get(2)+","+DeleteHewan.get(3)+"");
+            DeleteHewan.remove(1);
+            DeleteHewan.remove(1);
+            DeleteHewan.remove(1);
+            
+            System.out.println("After");
             System.out.println(DeleteHewan);
         }
 }
+
